57ea4b346a6ab286f784a990ac4a84d88742f623
Stephan Backenkoehler <s.backenkoehler@gmail.com>
2015-01-21 21:36:41 +0100

    giant swarm config update

--- cfmredirect/urls.py
+++ cfmredirect/urls.py
@@ -4,11 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^(?P<slug>\w+)/$', 'redirect.views.redirect', name='redirect'),
-    #url(r'^(?P<slug>\w+)$', 'redirect.views.redirect', name='redirect'),
-    # url(r'^blog/', include('blog.urls')),
 )
 urlpatterns += patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
